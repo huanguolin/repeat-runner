dc7ac482e300e8937b11ffc304c025324d334124
guolin.huang <guolin.huang@advantech.com.cn>
2017-04-21 09:56:19 +0800

    update dist

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /*!
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * RepeatRunner
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * (c) 2017 Alvin Huang
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * Released under the MIT License.
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      */
-
-require('babel-polyfill');
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/*!
+ * RepeatRunner
+ * 
+ * (c) 2017 Alvin Huang
+ * Released under the MIT License.
+ */
 
 var _ = new WeakMap();
 
@@ -76,7 +76,7 @@ var RepeatRunner = function () {
 
 
     _createClass(RepeatRunner, [{
-        key: 'start',
+        key: "start",
 
 
         /**
@@ -94,12 +94,12 @@ var RepeatRunner = function () {
          */
 
     }, {
-        key: 'stop',
+        key: "stop",
         value: function stop() {
             _.get(this).method.cancel();
         }
     }, {
-        key: 'isRunning',
+        key: "isRunning",
         get: function get() {
             return _.get(this).state.isRunning;
         }
