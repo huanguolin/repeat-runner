3f12e5460b074b74511fee94d0d14ff4cd69bf28
guolin.huang <guolin.huang@advantech.com.cn>
2017-04-24 18:58:27 +0800

    add test, and finished part case

--- test/repeat-runner.spec.js
+++ test/repeat-runner.spec.js
@@ -1,30 +1,98 @@
 import chai from 'chai';
-import RepeatRunner from '../src/index';
+import RepeatRunner from '../src/repeat-runner';
 
 const expect = chai.expect;
 
-describe('Repeater basic', () => {
+describe('RepeatRunner#constructor', () => {
 
-    function delayPrint (msg, delay) {
-        return new Promise( (resolve, reject) => {
-            setTimeout( () => {
-                console.log(msg);
-                resolve(delay);
-            }, delay);
-        });
-    }
+    it('all right', () => {
+        let instance;
 
-    let repeater;
+        expect(() => {
+            instance = new RepeatRunner( () => {}, 500);
+        }).to.not.throw(Error);
 
-    beforeEach( () => {
-        repeater = new RepeatRunner( () => { 
-            return delayPrint('hello', 500); 
-        }, 1000);
+        expect(instance).to.have.property('isRunning');
+        expect(typeof instance.start).to.be.equal('function');
+        expect(typeof instance.stop).to.be.equal('function');
     });
 
-    it('loop', () => {
-        repeater.start();
-        expect(repeater.isRunning).to.be.true;
+    it('omit second parameter is ok', () => {
+        expect(() => new RepeatRunner( () => {})).to.not.throw(Error);
+    });
+    
+    it('can not omit all parameter', () => {
+        expect(() => new RepeatRunner()).to.be.throw(Error);
+    });
+
+    it('frist parameter should be function', () => {
+        // array 
+        expect(() => new RepeatRunner([], 500)).to.be.throw(Error);
+        // object
+        expect(() => new RepeatRunner({}, 500)).to.be.throw(Error);
+        // number
+        expect(() => new RepeatRunner(123, 500)).to.be.throw(Error);
+        // string
+        expect(() => new RepeatRunner('abc', 500)).to.be.throw(Error);
+        // boolean
+        expect(() => new RepeatRunner(true, 500)).to.be.throw(Error);
+    });
+
+}); 
+
+describe('RepeatRunner.isRunning', () => {
+
+    const INTERVAL = 100; //0.1S
+    let instance, 
+        cnt;
+
+    beforeEach( () => {
+        cnt = 0;
+        instance = new RepeatRunner( () => cnt++, INTERVAL); 
+    });
+
+    afterEach( () => {
+        instance.stop();
+        instance = null;
+    });
+
+
+    it('not start, should be running', () => {
+        expect(instance.isRunning).to.be.false;
+    });
+
+    it('start it and should be running', () => {
+        instance.start();
+        expect(instance.isRunning).to.be.true;
+    });
+
+    it('stop it and should be not running', () => {
+        instance.start();
+        instance.stop();
+        expect(instance.isRunning).to.be.false;
+    });
+
+    it(`start and stop, the 'execFunction' 
+        should be executed at least once`, 
+        () => {
+        instance.start();
+        instance.stop();
+        expect(cnt > 0).to.be.true;
+    });
+
+    it(`interval is ${INTERVAL}ms, run 3*${INTERVAL}ms, 
+        the 'execFunction' should be executed 1+3 times`, 
+        done => {
+        console.log('start: ', cnt);
+        instance.start();
+
+        setTimeout( () => {
+            instance.stop();
+            console.log('stop: ', cnt);
+
+            if (cnt === 4) done();
+            else done(new Error());
+        }, INTERVAL * 3);
     });
 
 }); 
